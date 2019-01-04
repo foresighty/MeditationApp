b945436fa370fa19466aada2504dff26b23508a0
Chuck Taylor <chuckntaylor@gmail.com>
2019-01-04 14:48:58 -0500

    styled-components added

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Image } from 'react-native'
+import styled from 'styled-components/native'
+
+const CraneLogoImage = styled.Image`
+  width: 50%;
+  height: 50%;
+`
 
 class AboutScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Image
+        <CraneLogoImage
           source={require('../resources/images/crane-medicine-logo.png')}
         />
       </View>
