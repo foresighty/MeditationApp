4ffb2ba7afb5ef1e4ae03cdcef5a7f21389dbd84
Chuck Taylor <chuckntaylor@gmail.com>
2019-01-07 22:18:20 -0500

    Add stylesheet

---
+++ utils/cssGlobalStyles.js
@@ -0,0 +1,10 @@
+// Place to store reusable style values such as color
+
+const cssGlobalStyles = {
+  primaryBackgroundColor: '#4850AF',
+  activeTint: '#E06862',
+  inactiveTint: '#BBB',
+  sliderTint: '#424682'
+}
+
+export default cssGlobalStyles
